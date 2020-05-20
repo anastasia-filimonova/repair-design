d0485e7f42b1b6f7e1057014299c10b0f104a134
anastasia-filimonova <anastasia.filimonova@list.ru>
2020-05-20 18:21:00 +0300

    feat: add js script for modal window

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function(event) { 
   const modal = document.querySelector('.modal');
   const modalBtn = document.querySelectorAll('[data-toggle=modal]');
-  const closeBtn = documen.querySelector('.modal__close');
+  const closeBtn = document.querySelector('.modal__close');
   const switchModal = () => {
     modal.classList.toggle('modal--visible');
   } 
