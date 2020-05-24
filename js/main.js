4faa4ce4e03a55b4e831a6009093a9c127b1eabe
anastasia-filimonova <anastasia.filimonova@list.ru>
2020-05-24 12:51:59 +0300

    feat: add validation and phone masc / need some refact

--- js/main.js
+++ js/main.js
@@ -31,6 +31,84 @@ $(document).ready(function () {
 
   new WOW().init();
 
+  //Валидация формы
+  $('.modal__form').validate({
+    errorClass: "invalid",
+    rules: {
+      // simple rule, converted to {required:true} 
+      userName: {
+        required: true,
+        minlength: 2,
+        maxlength: 15
+      }, 
+      userPhone: "required",
+      // compound rule
+      userEmail: {
+        required: true,
+        email: true
+      }
+    },
+    messages: {
+      userName: {
+        required: "Заполните поле",
+        minlength: "Имя не короче двух букв"
+      },
+      userPhone: "Заполните поле",
+      userEmail: {
+        required: "Заполните поле",
+        email: "Введите корректный email"
+      }
+    }
+  })
+
+  $('.control__form').validate({
+    errorClass: "invalid",
+    rules: {
+      userName: {
+        required: true,
+        minlength: 2,
+        maxlength: 15
+      }, 
+      userPhone: {
+        required: true,
+        minlength: 2
+      } 
+    },
+    messages: {
+      userName: {
+        required: "Заполните поле",
+        minlength: "Имя не короче двух букв"
+      },
+      userPhone: "Заполните поле"
+    },
+  })
+
+  $('.footer__form').validate({
+    errorClass: "invalid",
+    rules: {      
+      userName: {
+        required: true,
+        minlength: 2,
+        maxlength: 15
+      }, 
+      userPhone: "required",
+      
+    },
+    messages: {
+      userName: {
+        required: "Заполните поле",
+        minlength: "Имя не короче двух букв"
+      },
+      userPhone: {
+        required: "Заполните поле",
+      }
+    }
+  })
+      // маска для телефона
+      $('[type=tel-control]').mask('+7(000) 00-00-000', {placeholder: "+7 (___) __-__-___"});
+      $('[type=tel-footer]').mask('+7(000) 00-00-000', {placeholder: "+7 (___) __-__-___"});
+      $('[type=tel-modal]').mask('+7(000) 00-00-000', {placeholder: "+7 (___) __-__-___"});
+      
 });
 
 $(document).ready(function(){
