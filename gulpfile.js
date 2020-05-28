73ac6bbfacc4771b1b606baf7ef68d179e2f95df
anastasia-filimonova <anastasia.filimonova@list.ru>
2020-05-28 23:09:49 +0300

    feat: creat dist: / minify js, css, html; optimized img

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
-const {src, dest, watch} = require('gulp');
+const {src, dest, watch, series} = require('gulp');
 const browserSync = require('browser-sync').create();
 const sass = require('gulp-sass');
-var cleanCSS = require('gulp-clean-css');
-var rename = require('gulp-rename');
+const cleanCSS = require('gulp-clean-css');
+const rename = require('gulp-rename');
 const autoprefixer = require('gulp-autoprefixer');
+const minify = require('gulp-minify');
+const htmlmin = require('gulp-htmlmin');
 
 function bs() { 
   serveSass(); 
@@ -28,10 +30,33 @@ function serveSass() {
     .pipe(browserSync.stream()); 
 };
 
+
+function buildCSS(done) {
+    src('./src/css/**/**.css')
+        .pipe(cleanCSS({ compatibility: 'ie8' }))
+        .pipe(dest('./dist/css'));
+    done();
+}
+  
+function buildJS(done) {
+    src(['./src/js/**.js', '!./src/js/**.min.js'])
+      .pipe(minify({ext:{
+              min:'.js'
+          }
+      }))
+      .pipe(dest('./dist/js'));
+    src('./src/js/**.min.js').pipe(dest('./dist/js'));
+    done();
+}
+
+function html(done) {
+  src('./src/*.html')
+      .pipe(htmlmin({ collapseWhitespace: true }))
+      .pipe(dest('./dist'));
+  done();
+}
+
+
 exports.serve = bs;
-
-
-function buildCSS(done)
-  src('css/**/**.css').pipe(cleanCSS({compatibility}))
-
+exports.build = series(buildCSS, buildJS, html);
 
